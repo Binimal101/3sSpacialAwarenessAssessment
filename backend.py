4e5aa3f068034a31338d4bc0dcefc2d8bf0775dd
VictorJimenez3 <154997712+VictorJimenez3@users.noreply.github.com>
2024-10-26 13:31:12 -0400

    I made the basic html webpages

---
+++ backend.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, render_template, request, url_for
+
+# flask app
+app = Flask(__name__)
+
+#app route
+@app.route('/')
+def home():
+    return render_template('index.html'); #make dynamic later
+
+@app.route('/regular')
+def regular():
+    return render_template('regular.html')
+
+@app.route('/TimeSeriesResults')
+def timeseries_results():
+    return render_template('timeseries_results.html')
+
+@app.route('/FinalResults')
+def final_results():
+    return render_template('final_results.html')
